7fad7d2f1937a57cf057f132696b3bd58b5a6dab
fadhil03 <mfhisyam5@gmail.com>
2023-08-03 22:06:42 +0800

    Revert "Revert "Update Panel 7""
    This reverts commit 70a3d121135a0c54f02262b48a9d244ff91ff78b.

--- Assets/Script/PartitionManager.cs
+++ Assets/Script/PartitionManager.cs
@@ -7,12 +7,17 @@ public class PartitionManager : MonoBehaviour
 {
     public GameObject scrollViewContent;
     public GameObject buttonTemplate;
-    private int unallocatedSpaceInGB = 500;
+    private int[] possibleSizes = { 128, 256, 512, 1024 };
+    private int unallocatedSpaceInGB;
     private int newButtonCount = 0; // Melacak berapa kali button "ButtonNew" ditekan.
 
     // Start is called before the first frame update
     void Start()
     {
+        int randomIndex = Random.Range(0, possibleSizes.Length);
+        unallocatedSpaceInGB = possibleSizes[randomIndex];
+        Debug.Log("Unallocated Space: " + unallocatedSpaceInGB + " GB");
+
         Button newButton = GameObject.Find("ButtonNew").GetComponent<Button>();
         newButton.onClick.AddListener(OnNewButtonClicked);
     }
@@ -28,14 +33,13 @@ public class PartitionManager : MonoBehaviour
         if (int.TryParse(sizeInputField.text, out int partitionSizeInMB))
         {
             // Konversi dari MB ke GB
-            int partitionSizeInGB = partitionSizeInMB / 1000;
+            int partitionSizeInGB = partitionSizeInMB / 1024;
 
             // Cek apakah ukuran partisi yang dimasukkan tidak melebihi Unallocated Space yang tersedia
             if (partitionSizeInGB <= unallocatedSpaceInGB)
             {
                 CreateNewButton(partitionSizeInGB);
                 unallocatedSpaceInGB -= partitionSizeInGB; // Kurangi Unallocated Space berdasarkan ukuran partisi yang baru dibuat
-                Debug.Log("Sisa Unlocaled: " + unallocatedSpaceInGB);
             }
             else
             {
@@ -51,7 +55,18 @@ public class PartitionManager : MonoBehaviour
     void CreateNewButton(int partitionSizeInGB)
     {
         GameObject newButton = Instantiate(buttonTemplate, scrollViewContent.transform);
-        newButton.GetComponentInChildren<Text>().text = "Partition Size: " + partitionSizeInGB + " GB";
+
+        // Akses komponen-komponen text pada button baru dan atur teksnya sesuai dengan data yang diinginkan
+        Text tvNamePartition = newButton.transform.Find("tvNamePartition").GetComponent<Text>();
+        Text tvTotalSize = newButton.transform.Find("tvTotalSize").GetComponent<Text>();
+        Text tvFreeSpace = newButton.transform.Find("tvFreeSpace").GetComponent<Text>();
+        Text tvTypePartition = newButton.transform.Find("tvTypePartition").GetComponent<Text>();
+
+        tvNamePartition.text = "Partition " + newButtonCount;
+        tvTotalSize.text = partitionSizeInGB + " GB";
+        tvFreeSpace.text = partitionSizeInGB + " GB";
+        tvTypePartition.text = "Type: NTFS";
+
         newButtonCount++;
     }
 }
