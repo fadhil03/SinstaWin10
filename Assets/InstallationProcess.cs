faae671412c1a4f00710cae49b679b1dbd48de97
fadhil03 <mfhisyam5@gmail.com>
2023-09-12 03:58:54 +0800

    Update Panel 8
    + Loading persentase loading progress
    Panel 8 ok

--- Assets/InstallationProcess.cs
+++ Assets/InstallationProcess.cs
@@ -15,13 +15,18 @@ public class InstallationProcess : MonoBehaviour
     public GameObject nextPanel;
     public GameObject currentPanel;
 
-    private int copyingFilesStartTime = 1; // 5 menit dalam detik
+    private int copyingFilesStartTime = 0; // 5 menit dalam detik
     private int gettingFilesStartTime = 15; // 10 menit dalam detik
     private int installingFeaturesStartTime = 20; // 6 menit dalam detik
-    private int finisihingaUpStartTime = 25;
+    private int finishingUpStartTime = 25;
     private int endStartTime = 30;
 
-    private void Start()
+    public Text progressTextCopyingFiles;
+    public Text progressTextGettingFiles;
+    public Text progressTextInstallingFeatures;
+    public Text progressTextFinishingUp;
+
+    private IEnumerator Start()
     {
         // Set teks awal yang tidak bold
         SetTextBold(copyingFiles, false);
@@ -32,66 +37,71 @@ public class InstallationProcess : MonoBehaviour
         imageComplete2.SetActive(false);
         imageComplete3.SetActive(false);
         imageComplete4.SetActive(false);
+
+        // Memulai proses loading untuk masing-masing teks dan gambar
+        StartCoroutine(StartLoading(copyingFiles, copyingFilesStartTime, imageComplete1, progressTextCopyingFiles, 15)); // Durasi loading 100 detik
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(StartLoading(gettingFiles, gettingFilesStartTime, imageComplete2, progressTextGettingFiles, 5)); // Durasi loading 200 detik
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(StartLoading(installingFeatures, installingFeaturesStartTime, imageComplete3, progressTextInstallingFeatures, 5)); // Durasi loading 150 detik
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(StartLoading(finishingUp, finishingUpStartTime, imageComplete4, progressTextFinishingUp, 3)); // Durasi loading 180 detik
+
+        StartCoroutine(DelayedPanelSwitch());
     }
 
     private void Update()
     {
         int currentTime = (int)Time.time;
         Debug.Log("Nilai current time adalah: " + currentTime);
+    }
 
-        // Cek apakah copyingFiles perlu dibuat bold dan menghilang
-        if (currentTime >= copyingFilesStartTime)
-        {
-            SetTextBold(copyingFiles, true);
-            SetTextBold(gettingFiles, false);
-            SetTextBold(installingFeatures, false);
-            SetTextBold(finishingUp, false);
-            // ...
-        }
+    private IEnumerator StartLoading(Text text, int startTime, GameObject image, Text progressText, int loadingDuration)
+    {
+        yield return new WaitForSeconds(startTime);
 
-        // Cek apakah gettingFiles perlu dibuat bold dan menghilang
-        if (currentTime >= gettingFilesStartTime)
-        {
-            SetTextBold(copyingFiles, false);
-            SetTextBold(gettingFiles, true);
-            SetTextBold(installingFeatures, false);
-            SetTextBold(finishingUp, false);
-            imageComplete1.SetActive(true);
-            // ...
-        }
+        // Membuat teks menjadi tidak bold dan menyembunyikan gambar
+        //SetTextBold(text, false);
+        //image.SetActive(false);
 
-        // Cek apakah installingFeatures perlu dibuat bold dan menghilang
-        if (currentTime >= installingFeaturesStartTime)
-        {
-            SetTextBold(copyingFiles, false);
-            SetTextBold(gettingFiles, false);
-            SetTextBold(installingFeatures, true);
-            SetTextBold(finishingUp, false);
-            imageComplete2.SetActive(true);
-            // ...
-        }
+        int currentTime = 0;
 
-        // Cek apakah installingFeatures perlu dibuat bold dan menghilang
-        if (currentTime >= finisihingaUpStartTime)
+        while (currentTime <= loadingDuration)
         {
-            SetTextBold(copyingFiles, false);
-            SetTextBold(gettingFiles, false);
-            SetTextBold(installingFeatures, false);
-            SetTextBold(finishingUp, true);
-            imageComplete3.SetActive(true);
-            // ...
-        }
+            // Hitung persentase progress
+            float progress = (float)currentTime / (float)loadingDuration * 100f;
+            // Tampilkan persentase progress
+            progressText.text = Mathf.Round(progress) + "%";
+            Debug.Log("progres: " + progress);
 
-        if (currentTime >= endStartTime)
-        {
-            SetTextBold(copyingFiles, false);
-            SetTextBold(gettingFiles, false);
-            SetTextBold(installingFeatures, false);
-            SetTextBold(finishingUp, false);
-            imageComplete4.SetActive(true);
-            StartCoroutine(DelayedExecution());
-            // ...
+            if (currentTime == 0)
+            {
+                // Proses loading dimulai, set teks menjadi tebal dan tampilkan gambar
+                SetTextBold(text, true);
+                image.SetActive(false);
+            }
+
+            yield return new WaitForSeconds(1f); // Tunggu selama 1 detik
+            currentTime++;
         }
+        //yield return new WaitForSeconds(1f);
+        // Proses loading selesai, set teks menjadi tidak bold dan menyembunyikan gambar
+        SetTextBold(text, false);
+        image.SetActive(true);
+
+        // Menghilangkan teks progress
+        progressText.text = "";
+    }
+
+
+    private IEnumerator DelayedPanelSwitch()
+    {
+        // Tunggu selama 5 detik
+        yield return new WaitForSeconds(30f);
+
+        // Nonaktifkan currentPanel dan aktifkan nextPanel
+        currentPanel.SetActive(false);
+        nextPanel.SetActive(true);
     }
 
     private void SetTextBold(Text text, bool isBold)
@@ -100,16 +110,5 @@ public class InstallationProcess : MonoBehaviour
         text.fontStyle = style;
     }
 
-    private IEnumerator DelayedExecution()
-    {
-        Debug.Log("Menggunakan coroutine: Menunda eksekusi selama 2 detik");
-
-        // Menunggu selama 2 detik
-        yield return new WaitForSeconds(5f);
-        currentPanel.SetActive(false);
-        nextPanel.SetActive(true);
-        // Kode yang akan dieksekusi setelah penundaan
-        Debug.Log("Kode setelah penundaan 2 detik");
-
-    }
+    // ...
 }
