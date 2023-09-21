aa064e28f4e521516a6a7a4a4e5f4a061e158732
fadhil03 <mfhisyam5@gmail.com>
2023-09-22 02:18:31 +0800

    Update Scene Bios
    Fixed bug langsung cek bootable setelah scene bios di load

--- Assets/BootingBios.cs
+++ Assets/BootingBios.cs
@@ -4,47 +4,48 @@ using UnityEngine;
 
 public class BootingBios : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public GameObject BIOSLogo; // Drag your BIOS logo GameObject here
+    public GameObject BIOSLogo;
     public GameObject F2text;
     public GameObject BootCanvas;
     public GameObject BiosCanvas;
     public GameObject CheckingCanvas;
 
-    private IEnumerator Start()
+    public float startTime = 0;
+
+    void Awake()
     {
         BIOSLogo.SetActive(false);
         F2text.SetActive(false);
-        BiosCanvas.SetActive(false); 
+        BiosCanvas.SetActive(false);
         CheckingCanvas.SetActive(false);
+    }
+
+    private IEnumerator Start()
+    {
         yield return new WaitForSeconds(2f);
         BIOSLogo.SetActive(true);
         F2text.SetActive(true);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        startTime += Time.deltaTime;
         if (Input.GetKeyDown(KeyCode.F2))
         {
             EnterBIOS();
             Debug.Log("F2 pressed!");
         }
 
-        if (Time.time > 8)
+        if (startTime > 8)
         {
             BootCanvas.SetActive(false);
             CheckingCanvas.SetActive(true);
         }
-
     }
-
     public void EnterBIOS()
     {
         BootCanvas.SetActive(false);
         BiosCanvas.SetActive(true);
         Debug.Log("Entering BIOS...");
     }
-
-
 }
--- Assets/CheckingBoot.cs
+++ Assets/CheckingBoot.cs
@@ -6,21 +6,23 @@ using UnityEngine.UI;
 
 public class CheckingBoot : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject noBootable;
     public GameObject reading;
     public GameObject enterToRestart;
-    void Start()
+
+    public float startTime = 0;
+
+    void Awake()
     {
         noBootable.SetActive(false);
         enterToRestart.SetActive(false);
         Debug.Log("Namascene : " + SceneManager.GetActiveScene().name);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Time.time > 15)
+        startTime += Time.deltaTime;
+        if (startTime > 15)
         {
             Debug.Log("selesai menunggu");
             reading.SetActive(false);
@@ -29,8 +31,7 @@ public class CheckingBoot : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
             {
                 Debug.Log("Namascene : " + SceneManager.GetActiveScene().name);
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-                //RestartScene();
+                RestartScene();
                 Debug.Log("=================selesai restart======================= ");
             }
         }
@@ -38,11 +39,7 @@ public class CheckingBoot : MonoBehaviour
 
     public void RestartScene()
     {
-        // Mendapatkan nama scene yang sedang aktif
         string currentSceneName = SceneManager.GetActiveScene().name;
-
-        // Memuat ulang scene yang sedang aktif
-        SceneManager.LoadScene("Bios");
+        SceneManager.LoadScene("InBootable");
     }
-
 }
