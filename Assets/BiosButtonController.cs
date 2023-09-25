b3a184157ccfb0c6031bed40acbd8f6acae99868
fadhil03 <mfhisyam5@gmail.com>
2023-09-25 12:38:27 +0800

    Create Main Panel (Bios)
    + Main panel bios
    1/8 navigation menu

---
+++ Assets/BiosButtonController.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BiosButtonController : MonoBehaviour
+{
+    public Button[] buttons; // Drag and drop all 5 buttons here in the inspector
+
+    private Color selectedButtonColor = new Color32(0xBB, 0xBB, 0xBB, 0xFF);
+    private Color unselectedButtonColor = new Color32(0x00, 0x00, 0xAA, 0xFF);
+    private Color highlightedButtonColor = new Color32(0xBB, 0xBB, 0xBB, 0xFF);
+    private Color selectedTextColor = new Color32(0x00, 0x00, 0xAA, 0xFF); // This is the color for selected text
+    private Color unselectedTextColor = new Color32(0xBB, 0xBB, 0xBB, 0xFF);
+    private Color highlightedTextColor = new Color32(0x00, 0x00, 0xAA, 0xFF);
+
+    private void Start()
+    {
+        foreach (Button button in buttons)
+        {
+            button.onClick.AddListener(() => OnButtonClicked(button));
+        }
+    }
+
+    private void OnButtonClicked(Button clickedButton)
+    {
+        foreach (Button button in buttons)
+        {
+            if (button == clickedButton)
+            {
+                SetButtonColor(button, selectedButtonColor, selectedTextColor); // The text color is set to 0000AA when selected
+            }
+            else
+            {
+                SetButtonColor(button, unselectedButtonColor, unselectedTextColor);
+            }
+        }
+    }
+
+    private void SetButtonColor(Button button, Color buttonColor, Color textColor)
+    {
+        ColorBlock colorBlock = button.colors;
+        colorBlock.normalColor = buttonColor;
+        colorBlock.highlightedColor = highlightedButtonColor;
+        button.colors = colorBlock;
+
+        Text buttonText = button.GetComponentInChildren<Text>();
+        if (buttonText)
+        {
+            buttonText.color = textColor;
+        }
+
+        // Change text color when button is highlighted
+        EventTrigger eventTrigger = button.gameObject.AddComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerEnter;
+        entry.callback.AddListener((eventData) => { buttonText.color = highlightedTextColor; });
+        eventTrigger.triggers.Add(entry);
+
+        entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerExit;
+        entry.callback.AddListener((eventData) => { buttonText.color = textColor; });
+        eventTrigger.triggers.Add(entry);
+    }
+}
