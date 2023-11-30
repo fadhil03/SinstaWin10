bbfdfc38e4004cf188f8b2fef26518aa59683967
fadhil03 <mfhisyam5@gmail.com>
2023-12-01 01:24:12 +0800

    Add Progress Bar 1
    + Progress bar Collecting Information

---
+++ Assets/ProgressBarController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarController : MonoBehaviour
+{
+    public Image foregroundImage;
+    public Image backgroundImage;
+    public GameObject[] loadingPanels;
+
+    void OnEnable()
+    {
+        // Panggil fungsi simulasi loading ketika game object diaktifkan
+        StartCoroutine(SimulateLoading());
+    }
+
+    void Start()
+    {
+        // Inisialisasi progres
+        float progress = 0.0f;
+        UpdateLoading(progress);
+    }
+
+    private void Update()
+    {
+        // Tidak perlu menambahkan progres di sini
+    }
+
+    IEnumerator SimulateLoading()
+    {
+        float totalProgress = 1.0f; // Total progres yang diinginkan
+        float panelIncrement = totalProgress / loadingPanels.Length; // Increment progres per panel
+
+        foreach (var panel in loadingPanels)
+        {
+            // Tunggu panel aktif
+            yield return new WaitUntil(() => panel.activeSelf);
+
+            // Tambahkan progres
+            UpdateLoading(foregroundImage.fillAmount + panelIncrement);
+
+            yield return null; // Jeda untuk memberi kesempatan update UI
+        }
+    }
+
+    void UpdateLoading(float progress)
+    {
+        // Atur tampilan foreground, background, atau panel-panel sesuai dengan progres
+        foregroundImage.fillAmount = Mathf.Clamp01(progress);
+    }
+}
