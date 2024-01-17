514a112683fc1033010f1c40cddbcebb4c0a8ae6
fadhil03 <mfhisyam5@gmail.com>
2024-01-17 12:43:06 +0800

    Update button next product key
    + when disable button script

---
+++ Assets/ButtonColorChange.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonColorChange : MonoBehaviour
+{
+    private Button button;
+    private Text buttonText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Mendapatkan referensi ke Button dan Text pada GameObject ini
+        button = GetComponent<Button>();
+        buttonText = GetComponentInChildren<Text>();
+
+        // Memanggil fungsi untuk mengatur warna awal
+        UpdateTextColor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Memanggil fungsi untuk memperbarui warna teks berdasarkan interactivity status button
+        UpdateTextColor();
+    }
+
+    void UpdateTextColor()
+    {
+        // Jika button tidak interactable, atur warna teks ke abu-abu
+        // Jika button interactable, atur warna teks ke hitam
+        if (button.interactable)
+        {
+            buttonText.color = Color.black;
+        }
+        else
+        {
+            buttonText.color = new Color(114 / 255f, 110 / 255f, 112 / 255f); // Warna abu-abu (726E70)
+        }
+    }
+}
